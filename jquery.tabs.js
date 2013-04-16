e239890443348666ab0dabdf3baf7f176e9eb456
Mahesh Sankhala <mahesh.saini@srijan.in>
2013-04-16 17:19:22 +0530

    jquery tabs with location hash change

---
+++ jquery.tabs.js
@@ -0,0 +1,34 @@
+jQuery.fn.tabs = function(control){
+  var element = $(this);
+  control = $(control);
+  
+  element.delegate("li", "click", function(){ 
+    // Retrieve tab name
+    var tabName = $(this).attr("data-tab");
+    // Fire custom event on tab click    
+    element.trigger("change.tabs", tabName);
+  });
+  // Bind to custom event 
+  element.bind("change.tabs", function(e, tabName){
+  element.find("li").removeClass("active");
+  element.find(">[data-tab='" + tabName + "']").addClass("active"); });
+  
+  element.bind("change.tabs", function(e, tabName){   
+  control.find(">[data-tab]").removeClass("active");
+  control.find(">[data-tab='" + tabName + "']").addClass("active");
+  });
+  var firstName = element.find("li:first").attr("data-tab"); 
+  element.trigger("change.tabs", firstName);
+  return this;
+};
+$(function(){
+  $("ul#tabs").tabs("#container");
+
+  //change the hash of window.
+  $("#tabs").bind("change.tabs", function(e, tabName){ window.location.hash = tabName;
+  });
+
+  $(window).bind("hashchange", function(){
+  var tabName = window.location.hash.slice(1); $("#tabs").trigger("change.tabs", tabName);
+  });
+});
